253448764ce1fb06b00f5deb864ea13e27876c71
isemin <isemin@outlook.com>
2017-02-07 00:02:48 +0300

    * author form

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -16,7 +16,8 @@ var AuthorForm = React.createClass({
 					className="form-control"
 					placeholder="First Name"
 					ref="firstName"
-					value="" />
+					onChange={this.props.onChange}
+					value={this.props.author.firstName} />
 				<br />
 
 				<label htmlFor="lastName">First Name</label>
@@ -25,7 +26,8 @@ var AuthorForm = React.createClass({
 					className="form-control"
 					placeholder="Last Name"
 					ref="lastName"
-					value="" />
+					onChange={this.props.onChange}
+					value={this.props.author.lastName} />
 				<br />
 
 				<input type="submit" value="Save" className="btn btn-default" />
