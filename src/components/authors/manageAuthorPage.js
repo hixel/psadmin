45ed2bc5ad52e8baa2b61eadf6a4eeecb42ed4e9
isemin <isemin@outlook.com>
2017-02-09 22:33:04 +0300

    * save author

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -1,9 +1,16 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./authorForm');
+var AuthorApi = require('../../api/authorApi');
 
 var ManageAuthorPage = React.createClass({
+
+	mixins: [
+
+		Router.Navigation
+	],
 
 	getInitialState: function() {
 
@@ -24,12 +31,20 @@ var ManageAuthorPage = React.createClass({
 		});
 	},
 
+	saveAuthor: function(event) {
+
+		event.preventDefault();
+		AuthorApi.saveAuthor(this.state.author);
+		this.transitionTo('authors');
+	},
+
 	render: function() {
 
 		return (	
 			<AuthorForm 
 				author={this.state.author}
-				onChange={this.setAuthorState} />
+				onChange={this.setAuthorState}
+				onSave={this.saveAuthor} />
 		);
 	}
 });
